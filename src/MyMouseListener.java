7fca69e9e150640261ff93db5c8235fd1e04f904
jony <yhudashwartz2004@gmail.com>
2022-06-14 21:55:45 +0300

    a

--- src/MyMouseListener.java
+++ src/MyMouseListener.java
@@ -3,6 +3,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MyMouseListener implements MouseListener {
+private int counter = 0;
 
     private Panel panel;
 
@@ -12,10 +13,53 @@ public class MyMouseListener implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        this.counter++;
         System.out.println("mouseClicked" + e.getX());
         Shapes shape = new Shapes(e.getX(),e.getY(),50,50,Color.BLACK);
-        panel.Black(e.getX(),e.getY());
-        panel.Blue(e.getX(),e.getY());
+        if (counter == 1) {
+            panel.Black(e.getX(), e.getY());
+           // panel.Blue(e.getX(), e.getY());
+        }
+
+        if (counter == 2) {
+           // panel.Black2(e.getX(), e.getY());
+            panel.Blue2(e.getX(), e.getY());
+        }
+
+        if (counter == 3) {
+            panel.Black3(e.getX(), e.getY());
+           // panel.Blue3(e.getX(), e.getY());
+        }
+
+        if (counter == 4) {
+            panel.Blue4(e.getX(), e.getY());
+          //  panel.Blue4(e.getX(), e.getY());
+        }
+
+        if (counter == 5) {
+            panel.Black5(e.getX(), e.getY());
+          //  panel.Blue5(e.getX(), e.getY());
+        }
+
+        if (counter == 6) {
+            panel.Blue6(e.getX(), e.getY());
+        //    panel.Blue6(e.getX(), e.getY());
+        }
+
+        if (counter == 7) {
+            panel.Black7(e.getX(), e.getY());
+          //  panel.Blue7(e.getX(), e.getY());
+        }
+
+        if (counter == 8) {
+            panel.Blue8(e.getX(), e.getY());
+         //   panel.Blue8(e.getX(), e.getY());
+        }
+
+        if (counter == 9) {
+            panel.Black9(e.getX(), e.getY());
+         //   panel.Blue9(e.getX(), e.getY());
+        }
 
         panel.repaint();
     }
--- src/Panel.java
+++ src/Panel.java
@@ -9,6 +9,23 @@ public class Panel extends JPanel {
     private ImageIcon background;
     private Shapes rec;
     private ShapesBlack recBlack;
+    private Shapes rec2;
+    private ShapesBlack recBlack2;
+    private Shapes rec3;
+    private ShapesBlack recBlack3;
+    private Shapes rec4;
+    private ShapesBlack recBlack4;
+    private Shapes rec5;
+    private ShapesBlack recBlack5;
+    private Shapes rec6;
+    private ShapesBlack recBlack6;
+    private Shapes rec7;
+    private ShapesBlack recBlack7;
+    private Shapes rec8;
+    private ShapesBlack recBlack8;
+    private Shapes rec9;
+    private ShapesBlack recBlack9;
+
 
     public Panel() {
         this.setBounds(0, 0, Window_Width, Window_Height);
@@ -28,6 +45,54 @@ public class Panel extends JPanel {
         if (this.recBlack != null) {
             this.recBlack.paint(g);
         }
+        if (this.rec2 != null) {
+            this.rec2.paint(g);
+        }
+        if (this.recBlack2 != null) {
+            this.recBlack2.paint(g);
+        }
+        if (this.rec3 != null) {
+            this.rec3.paint(g);
+        }
+        if (this.recBlack3 != null) {
+            this.recBlack3.paint(g);
+        }
+        if (this.rec4 != null) {
+            this.rec4.paint(g);
+        }
+        if (this.recBlack4 != null) {
+            this.recBlack4.paint(g);
+        }
+        if (this.rec5 != null) {
+            this.rec5.paint(g);
+        }
+        if (this.recBlack5 != null) {
+            this.recBlack5.paint(g);
+        }
+        if (this.rec6 != null) {
+            this.rec6.paint(g);
+        }
+        if (this.recBlack6 != null) {
+            this.recBlack6.paint(g);
+        }
+        if (this.rec7 != null) {
+            this.rec7.paint(g);
+        }
+        if (this.recBlack7 != null) {
+            this.recBlack7.paint(g);
+        }
+        if (this.rec8 != null) {
+            this.rec8.paint(g);
+        }
+        if (this.recBlack8 != null) {
+            this.recBlack8.paint(g);
+        }
+        if (this.rec9 != null) {
+            this.rec9.paint(g);
+        }
+        if (this.recBlack9 != null) {
+            this.recBlack9.paint(g);
+        }
     }
 
     public void Black(double x, double y) {
@@ -39,6 +104,89 @@ public class Panel extends JPanel {
         this.recBlack = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
 
     }
+
+    public void Black2(double x, double y) {
+        this.rec2 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue2(double x, double y) {
+        this.recBlack2 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black3(double x, double y) {
+        this.rec3 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue3(double x, double y) {
+        this.recBlack3 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black4(double x, double y) {
+        this.rec4 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue4(double x, double y) {
+        this.recBlack4 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black5(double x, double y) {
+        this.rec5 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue5(double x, double y) {
+        this.recBlack5 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black6(double x, double y) {
+        this.rec6 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue6(double x, double y) {
+        this.recBlack6 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black7(double x, double y) {
+        this.rec7 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue7(double x, double y) {
+        this.recBlack7 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black8(double x, double y) {
+        this.rec8 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue8(double x, double y) {
+        this.recBlack8 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+    public void Black9(double x, double y) {
+        this.rec9 = new Shapes (x - 20,y - 20,20,20,Color.BLACK);
+
+    }
+
+    public void Blue9(double x, double y) {
+        this.recBlack9 = new ShapesBlack (x - 20,y - 20,20,40,Color.blue);
+
+    }
+
+
+
 
     public void mainGameLoop() {
         Thread thred = new Thread(() -> {
